0e845454d96a42c3c8cb21627b7ea02302f92ae2
Narigara007 <narigara007@gmail.com>
2023-10-02 12:08:04 +0530

    Initial commit

---
+++ SwiftRedux/SwiftRedux/SwiftReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftReduxApp.swift
+//  SwiftRedux
+//
+//  Created by Dipak on 02/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
